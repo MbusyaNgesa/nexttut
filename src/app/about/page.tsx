b53471b3704781555bda976a2e25fbb8db4d42c0
MbusyaNgesa <theengesa@gmail.com>
2024-12-12 12:46:16 +0300

    Nested/Dynamic Routes & Route Groups

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <h1
+      className="flex items-center justify-center
+     text-xl mt-2"
+    >
+      About Us
+    </h1>
+  );
+};
+
+export default About;
---
+++ src/app/blog/movies/page.tsx
@@ -0,0 +1,13 @@
+const Movies = () => {
+  return (
+    <>
+      <ul>
+        <li>Avengers</li>
+        <li>3 Body Problem</li>
+        <li>Alice in Borderland</li>
+      </ul>
+    </>
+  );
+};
+
+export default Movies;
